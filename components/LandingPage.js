ccd2deac4a46cd77d4678fd76f5295567787b37b
samduck150906-lgtm <samduck150906@gmail.com>
2026-03-30 02:51:57 +0900

    refactor: Paddle 결제 코드 완전 삭제 - 토스페이먼츠 전환 완료

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -4,8 +4,7 @@ import {
   getLandingBySlug,
   pickFaqs,
   pickReviews,
-  getPricingSnippet,
-} from '../lib/data';
+  getPricingSnippet } from '../lib/data';
 
 const BASE = process.env.NEXT_PUBLIC_SITE_URL || 'https://reumlab.com';
 
